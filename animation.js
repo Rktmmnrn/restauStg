f541865972ae418b48636be9f898a915a8c02a06
rktmmnrn <rfanomezaniavo@gmail.com>
2024-12-19 12:13:07 +0300

    last MAJ

--- animation.js
+++ animation.js
@@ -1,24 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let rightSect = document.querySelector('hero-right');
-  let sectRight = document.getElementById('menu-right');
-  sectRight.addEventListener('Click', () => {
-    console.log('click ohh');
-    rightSect.style.display = 'flex';
-    rightSect.style.transform = 'translateX(0%)';
-  })
+  const rightSect = document.querySelector('.hero-right');
+  const sectRight = document.querySelector('.menu-right');
 
   sectRight.addEventListener('click', () => {
     console.log('cliccckk');
-    let menu1 = document.getElementById('nav-menu1');
+    const menu1 = document.querySelector('.nav-menu-1');
 
     if (menu1.style.display = 'none') {
       menu1.style.display = 'flex';
       menu1.style.transform = 'translateX(0)';
+      menu1.style.transform = 'translateY(100%)';
     } else {
       menu1.style.display = 'none';
     }
   })
-
-  // queryselector : prendre les éléments class dans html avec JS
-
 })
